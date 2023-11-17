862ca89bb99f8d7d17e41427491b1cc62cb079e4
Odilon Martins da Silva Neto <omneto@blockbit.com>
2023-11-17 16:01:22 -0300

    ajuste build

--- src/Login.tsx
+++ src/Login.tsx
@@ -2,9 +2,9 @@ import {VStack, Image, Text, Box, FormControl, Input, Button, Link, useToast, Ic
 import { TouchableOpacity } from 'react-native';
 import { useEffect, useState } from 'react'
 import * as React from 'react';
-//import * as Google from 'expo-auth-session/providers/google';
-import * as AuthSession from 'expo-auth-session';
-import * as WebBrowser from 'expo-web-browser';
+
+//import * as AuthSession from 'expo-auth-session';
+//import * as WebBrowser from 'expo-web-browser';
 
 import Logo from "./assets/logo2.png"
 
@@ -12,61 +12,68 @@ import { logarService } from '../servicos/loginServico';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
-type AuthResponse = {
-  params: {
-    access_token: string;
-  };
-  type: string;
-}
+import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+
+// type AuthResponse = {
+//   params: {
+//     access_token: string;
+//   };
+//   type: string;
+// }
 
 export default function Login({ navigation }: any) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [carregando, setCarregando] = useState(true);
   const toast = useToast();
+
+  GoogleSignin.configure({
+    webClientId: '173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com',
+    offlineAccess: true
+  });
   
-  // const CLIENT_ID = '173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com';
-  // const REDIRECT_URI = "https://auth.expo.io/@odn3to/deliverFood";
-  // const SCOPE = encodeURI("profile email");
-  // const RESPONSE_TYPE = "token";
+  async function logarGoogle() {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
 
-  // const config = {
-  //   expoClientId: `173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com`,
-  //   iosClientId: `173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com`,
-  //   androidClientId: `173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com`,
-  //   webClientId: `173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com`,
-  // };
-
+      console.log(userInfo)
+      // userInfo.user contém as informações do usuário
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
   // const [request, response, promptAsync] = AuthSession.useAuthRequest(
   
-    const discovery = AuthSession.useAutoDiscovery('https://accounts.google.com');
+    // const discovery = AuthSession.useAutoDiscovery('https://accounts.google.com');
 
-    // Seu ClientId e Redirect URI
-    const clientId = '173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com';
-    const redirectUri = AuthSession.makeRedirectUri({
-      // useProxy: true, // Isso utilizará o proxy do Expo, que é necessário para desenvolvimento local
-      // native: 'com.googleusercontent.apps.173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg:/oauth2redirect/google', // Esquema do seu app
-      // For usage in bare and standalone
-      native: 'com.deliverfood.somethingelse://',
-    });
+    // // Seu ClientId e Redirect URI
+    // const clientId = '173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg.apps.googleusercontent.com';
+    // const redirectUri = AuthSession.makeRedirectUri({
+    //   // useProxy: true, // Isso utilizará o proxy do Expo, que é necessário para desenvolvimento local
+    //   // native: 'com.googleusercontent.apps.173314048003-j24c1d0ep554kekm77lmv2h9oalf5sdg:/oauth2redirect/google', // Esquema do seu app
+    //   // For usage in bare and standalone
+    //   native: 'com.deliverfood.somethingelse://',
+    // });
   
-    // Iniciar a solicitação de autenticação
-    const [request, response, promptAsync] = AuthSession.useAuthRequest(
-      {
-        clientId,
-        scopes: ['profile', 'email'],
-        redirectUri,
-      },
-      discovery
-    );
+    // // Iniciar a solicitação de autenticação
+    // const [request, response, promptAsync] = AuthSession.useAuthRequest(
+    //   {
+    //     clientId,
+    //     scopes: ['profile', 'email'],
+    //     redirectUri,
+    //   },
+    //   discovery
+    // );
   
-    React.useEffect(() => {
-      if (response?.type === 'success') {
-        const { authentication } = response;
-        console.log(authentication);
-        // Agora você tem um token de acesso que pode usar para fazer requisições para a API do Google
-      }
-    }, [response]);
+    // React.useEffect(() => {
+    //   if (response?.type === 'success') {
+    //     const { authentication } = response;
+    //     console.log(authentication);
+    //     // Agora você tem um token de acesso que pode usar para fazer requisições para a API do Google
+    //   }
+    // }, [response]);
 
 
   const logar = async() => {
@@ -74,17 +81,17 @@ export default function Login({ navigation }: any) {
     console.log(resultado);
   }
 
-  const logarGoogle = async() => {
-    try {
-      console.log("Iniciando login Google");
-      await promptAsync();
-    } catch (error) {
-      console.error(error);
-      toast.show({
-        description: "Erro ao tentar login com Google.",
-      });
-    }
-  }
+  // const logarGoogle = async() => {
+  //   try {
+  //     console.log("Iniciando login Google");
+  //     await promptAsync();
+  //   } catch (error) {
+  //     console.error(error);
+  //     toast.show({
+  //       description: "Erro ao tentar login com Google.",
+  //     });
+  //   }
+  // }
 
 
   return (
